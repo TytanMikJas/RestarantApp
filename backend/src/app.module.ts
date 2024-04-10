a1cf09dc5c4da0cf6f085c25ebee2dcd109f099e
wiktorstankiewicz <36522853+wiktorstankiewicz@users.noreply.github.com>
2024-04-10 21:50:26 +0200

    Serve statics (#2)
    * serve statics on /public

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import OrderModule from './modules/order/order.module';
 import MenuModule from './modules/menu/menu.module';
+import { join } from 'path';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
-  imports: [OrderModule, MenuModule],
+  imports: [OrderModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../../'),
+      renderPath: '/public',
+    }),
+     MenuModule],
+    
   providers: [],
 })
 export class AppModule {}
