5366bacb7b92ca7baba71559e4cb097c9f5dc7d3
TytanMikJas <mikjas02@gmail.com>
2024-04-10 17:45:54 +0200

    order agteway

--- backend/src/modules/order/order.gateway.ts
+++ backend/src/modules/order/order.gateway.ts
@@ -1,8 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  WebSocketGateway,
+} from '@nestjs/websockets';
+import OrderService from './order.service';
 
+@WebSocketGateway({ transports: ['websocket'], cors: true })
 @Injectable()
-export default class OrderGateway {
-  handleWaiterConnection() {
-    throw new Error('Method not implemented.');
+export default class OrderGateway
+  implements OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit
+{
+  constructor(private readonly oderdService: OrderService) {}
+
+  async afterInit(): Promise<void> {
+    console.log('init');
+  }
+
+  async handleConnection(client: any) {
+    console.log(client);
+  }
+
+  async handleDisconnect(client: any) {
+    console.log(client);
   }
 }
