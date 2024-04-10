9f8668a35f40adef9b85558b7ab8271045cda514
Wiktor <wiktorstankiewicz24@gmail.com>
2024-04-10 21:49:01 +0200

    serve statics on /public

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,14 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: false,
     optionsSuccessStatus: 204,
   });
+
   await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
