f079236abb0f072efc47112ca9953fcb2455dab4
Wiktor <wiktorstankiewicz24@gmail.com>
2024-04-22 18:07:28 +0200

    updated dependencies

--- AndroidApp/app/build.gradle.kts
+++ AndroidApp/app/build.gradle.kts
@@ -67,4 +67,6 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation(libs.retrofit.core)
+    implementation(libs.retrofit.gson.converter)
 }
---
+++ AndroidApp/app/src/main/java/com/example/androidapp/api/dto/MenuDto.java
@@ -0,0 +1,5 @@
+package com.example.androidapp.api.dto;
+
+public class MenuDto {
+    private Integer id;
+}
